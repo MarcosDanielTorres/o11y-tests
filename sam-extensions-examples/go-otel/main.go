e5866a1e050f8d6c7ad9bd225431773e67fa5111
Marcos Daniel Torres <marcoss.nqn@gmail.com>
2024-10-15 11:02:04 -0300

    added sending logs in otlp format

--- sam-extensions-examples/go-otel/main.go
+++ sam-extensions-examples/go-otel/main.go
@@ -1,21 +1,33 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"log/slog"
+	"net/http"
 	"time"
 
 	"go.opentelemetry.io/contrib/bridges/otelslog"
+	"google.golang.org/protobuf/proto"
 	//"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp"
+	otellog "go.opentelemetry.io/otel/log"
 	"go.opentelemetry.io/otel/log/global"
 	"go.opentelemetry.io/otel/propagation"
-	otelLog "go.opentelemetry.io/otel/sdk/log"
+	sdklog "go.opentelemetry.io/otel/sdk/log"
 	"go.opentelemetry.io/otel/sdk/resource"
+	v1 "go.opentelemetry.io/proto/otlp/collector/logs/v1" // Ensure correct
+
+	commonv1 "go.opentelemetry.io/proto/otlp/common/v1"
+
+	logs "go.opentelemetry.io/proto/otlp/logs/v1" // Import for ResourceLogs
+	protoresourcev1 "go.opentelemetry.io/proto/otlp/resource/v1"
 )
 
 const NearlyImmediate = 100 * time.Millisecond
@@ -55,7 +67,8 @@ func initOtel(ctx context.Context) (shutdown func(context.Context) error, err er
 	shutdownFuncs = append(shutdownFuncs, loggerProvider.Shutdown)
 	global.SetLoggerProvider(loggerProvider)
 
-	slog.SetDefault(otelslog.NewLogger("my/pkg/name", otelslog.WithLoggerProvider(loggerProvider)))
+	logger := otelslog.NewLogger("my/pkg/name", otelslog.WithLoggerProvider(loggerProvider))
+	slog.SetDefault(logger)
 
 	return
 }
@@ -73,15 +86,15 @@ func createResource() (*resource.Resource, error) {
 	return res, nil
 }
 
-func newLoggerProvider(resource *resource.Resource) (*otelLog.LoggerProvider, error) {
+func newLoggerProvider(resource *resource.Resource) (*sdklog.LoggerProvider, error) {
 	logExporter, err := otlploghttp.New(context.Background(), otlploghttp.WithInsecure(), otlploghttp.WithEndpointURL("http://localhost:4318/v1/logs"))
 	if err != nil {
 		return nil, fmt.Errorf("failed to create log exporter: %w", err)
 	}
-	processor := otelLog.NewBatchProcessor(logExporter, otelLog.WithExportInterval(NearlyImmediate))
-	loggerProvider := otelLog.NewLoggerProvider(
-		otelLog.WithResource(resource),
-		otelLog.WithProcessor(processor),
+	processor := sdklog.NewBatchProcessor(logExporter, sdklog.WithExportInterval(NearlyImmediate))
+	loggerProvider := sdklog.NewLoggerProvider(
+		sdklog.WithResource(resource),
+		sdklog.WithProcessor(processor),
 	)
 	return loggerProvider, nil
 }
@@ -92,6 +105,139 @@ func newPropagator() propagation.TextMapPropagator {
 		propagation.Baggage{},
 	)
 }
+func sendToLoki() {
+	// Define the JSON structure
+	data := map[string]interface{}{
+		"streams": []map[string]interface{}{
+			{
+				"stream": map[string]string{
+					"label": "value",
+				},
+				"values": [][]string{
+					{fmt.Sprintf("%d", time.Now().UnixNano()), "First log line"},
+					{fmt.Sprintf("%d", time.Now().UnixNano()), "Second log line"},
+				},
+			},
+		},
+	}
+
+	// Encode JSON data
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return
+	}
+
+	// Define the POST URL
+	url := "http://localhost:3100/loki/api/v1/push"
+
+	// Create a POST request with the JSON data
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	// Set the appropriate headers
+	req.Header.Set("Content-Type", "application/json")
+
+	// Send the request using the http.DefaultClient
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Check response status
+	if resp.StatusCode == http.StatusNoContent {
+		fmt.Println("Request successful")
+	} else {
+		fmt.Printf("Failed to send request, status code: %d\n", resp.StatusCode)
+	}
+}
+
+func sendToOtel() {
+	//data := map[string]interface{}{
+	//	"streams": []map[string]interface{}{
+	//		{
+	//			"stream": map[string]string{
+	//				"label": "value",
+	//			},
+	//			"values": [][]string{
+	//				{fmt.Sprintf("%d", time.Now().UnixNano()), "First log line"},
+	//				{fmt.Sprintf("%d", time.Now().UnixNano()), "Second log line"},
+	//			},
+	//		},
+	//	},
+	//}
+
+	request := &v1.ExportLogsServiceRequest{
+		ResourceLogs: []*logs.ResourceLogs{
+			{
+				Resource: &protoresourcev1.Resource{
+					Attributes: []*commonv1.KeyValue{
+						{Key: "service.name", Value: &commonv1.AnyValue{Value: &commonv1.AnyValue_StringValue{StringValue: "my-service"}}},
+					},
+				},
+				ScopeLogs: []*logs.ScopeLogs{
+					{Scope: &commonv1.InstrumentationScope{Name: "hola", Version: "1.1.1", Attributes: []*commonv1.KeyValue{
+						{Key: "log.attributes", Value: &commonv1.AnyValue{Value: &commonv1.AnyValue_StringValue{StringValue: "some-log-value"}}},
+					}}, LogRecords: []*logs.LogRecord{
+						{
+							SeverityNumber: logs.SeverityNumber_SEVERITY_NUMBER_FATAL,
+							Body:           &commonv1.AnyValue{Value: &commonv1.AnyValue_IntValue{IntValue: 322}},
+						},
+					}},
+				},
+			},
+		},
+	}
+
+	// Encode JSON data
+	jsonData, err := proto.Marshal(request)
+	if err != nil {
+		fmt.Println("Error marshaling JSON:", err)
+		return
+	}
+	url := "http://localhost:4318/v1/logs"
+
+	// Create a POST request with the JSON data
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	// Set the appropriate headers
+	//req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Type", "application/x-protobuf")
+
+	// Send the request using the http.DefaultClient
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Check response status
+	fmt.Println("OTEL response status code: ", resp.StatusCode)
+	fmt.Println("OTEL response header", resp.Header)
+	fmt.Println("OTEL response body", resp.Body)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic("Error reading body")
+	}
+	fmt.Println("Raw response body:", string(body))
+	var response v1.ExportLogsServiceResponse
+	//json.Unmarshal(body, &response)
+	proto.Unmarshal(body, &response)
+	fmt.Println("OTEL response body unmarshaled", response.String())
+
+}
 
 func main() {
 	ctx := context.WithoutCancel(context.Background())
@@ -100,11 +246,25 @@ func main() {
 		log.Fatal("Error initializing OpenTelemetry: ", err)
 	}
 
-	slog.Info("Info LOG")
-	slog.Debug("Debug LOG")
-	slog.Info("Info LOG")
-	slog.Warn("Warn LOG")
-	slog.Error("Error LOG")
+	exampleJson := []byte(`{"key1": "value1", "key2": "value2"}`)
+
+	rec := otellog.Record{}
+	rec.SetTimestamp(time.Now())
+	//rec.SetBody(otellog.StringValue("Hello, World!"))
+	//rec.SetBody(otellog.BytesValue(exampleJson))
+	rec.SetBody(otellog.StringValue(string(exampleJson)))
+	//global.GetLoggerProvider().Logger("--some-logger-name--").Emit(ctx, rec)
+
+	//slog.Info("Info LOG")
+	//slog.Debug("Debug LOG")
+	//slog.Info("Info LOG")
+	//slog.Warn("Warn LOG")
+	//slog.Error("Error LOG")
+
+	fmt.Println("Hello world how are you going?")
+
+	//sendToLoki()
+	sendToOtel()
 
 	shutdown(context.Background())
 }
